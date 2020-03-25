3781717519b3b95678415d32fdfbc5e049a86185
pjmantoss <joelptoss@gmail.com>
2020-03-25 18:59:23 +0100

    update script.js

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 
 const progressBar = player.querySelector('.progress-filled');
+
+const toggle = player.querySelector('.toogle');
+
+const skipBtns = player.querySelectorAll('[data-skip]');
+
+const ranges = player.querySelectorAll('.player-slider');
+
+/*Construct Functions*/
+
+togglePlay = () => {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+updateBtn = () => {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon
+}
