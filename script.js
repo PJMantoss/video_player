9d68469f70a9fc47dd70f0731abe278fa9b1d969
pjmantoss <joelptoss@gmail.com>
2020-03-25 18:49:24 +0100

    update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Select our Elements*/
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+
+const progressBar = player.querySelector('.progress-filled');
